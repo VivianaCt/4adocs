ff67038dcbb59189ecf12dcfaf614e64a1000f92
katialineth <94063300+katialineth@users.noreply.github.com>
2021-11-21 17:16:58 -0500

    Add files via upload
    Entrada al repositorio

---
+++ auth_app/views/userCreate.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from rest_framework import status, views 
+from rest_framework.response import Response 
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer 
+ 
+from auth_app.models.serializers.userSerializer import UserSerializer  
+ 
+class UserCreate(views.APIView): 
+    #debe llegar JSON con la data del user a registrar
+
+    def post(self, request, *args, **kwargs): 
+
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'No está autorizado para ver esto'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = UserSerializer(data=request.data)         
+        serializer.is_valid(raise_exception=True)         
+        serializer.save() 
+
+        tokenData = {"userName":request.data["userName"],                       
+                     "password":request.data["password"]}         
+        tokenSerializer = TokenObtainPairSerializer(data=tokenData)         
+        tokenSerializer.is_valid(raise_exception=True)                          
+        
+        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)
+        
