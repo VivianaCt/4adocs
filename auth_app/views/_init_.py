9ab8270b572eb46016d83cfa106725a9c0476e56
katialineth <94063300+katialineth@users.noreply.github.com>
2021-11-21 17:14:10 -0500

    Create _init_.py
    Ingreso al repositorio

---
+++ auth_app/views/_init_.py
@@ -0,0 +1,17 @@
+from .usuarios import CrearUsuarioView, DetalleUsuarioView
+from .login import LoginView, ManualLoginView, LoginCustomView, CheckToken
+from .login_simple_jwt import *
+
+#VISTAS DEL CRUD DE PRODUCTOS
+from .producCreateView import ProductCreateView
+from .productAllDetailView import ProductAllDetailView
+from .productDeleteView import ProductDeleteView
+from .productDetailView import ProductDetailView
+from .productUpdateView import ProductUpdateView
+
+#VISTA DEL CRUD USUARIO
+from .userCreate import UserCreate
+from .userDetail import UserDetail
+from .userAllDetail import UserAllDetail
+from .userDelete import UserDelete
+from .userUpdate import UserUpdate
