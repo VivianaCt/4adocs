624c16bbf0f9d507074d73e32e587af1323946ba
katialineth <94063300+katialineth@users.noreply.github.com>
2021-11-23 00:19:13 -0500

    Create adminDelete.py
    CRUD ADMIN

---
+++ auth_app/views/adminDelete.py
@@ -0,0 +1,25 @@
+from auth_app.models.admins import Admin 
+from auth_app.serializers.adminSerializer import AdminSerializer 
+ 
+class AdminDelete(views.APIView):     
+    queryset = Admin.objects.all()     
+    serializer_class = AdminSerializer     
+    permission_classes = (IsAuthenticated,)   
+
+    def delete(self, request, *args, **kwargs):
+        #debe llegar PK del user y su accen token
+        
+
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+
+        if valid_data['admin_id'] != kwargs['pk']:
+            stringResponse = {'detail':'No está autorizado para ver esto'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+
+        adminSerializer = AdminSerializer()
+
+        stringResponse = {'detail':'se elimino correctamente'}
+        return Response(adminSerializer.delete_element(id=kwargs["pk"]), status=status.HTTP_200_OK)
+      
